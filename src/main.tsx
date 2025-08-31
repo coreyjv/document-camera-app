526947f2c33d2896a0157ad9cd34f1fcca7e23db
Corey Vaillancourt <coreyjv@gmail.com>
2025-08-31 10:51:18 -0400

    Setup Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://c53e1463ba6774a19eadd17d04141f22@o4509939221397504.ingest.us.sentry.io/4509939234832384',
+  sendDefaultPii: false
+})
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,32 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
+      org: 'centinul',
+      project: 'doccam'
+    })
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   },
+
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './setup-tests.ts'
+  },
+
+  build: {
+    sourcemap: true
   }
 })
